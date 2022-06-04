06c7cdf4414c5b2beb8e987b6659888eee2a020e
Aman Kumar <amankumar.spj410@gmail.com>
2022-06-05 00:21:52 +0530

    ui improve

---
+++ public/common.js
@@ -0,0 +1,9 @@
+document.querySelector("#sitemap").onclick = e => {
+    e.preventDefault()
+    document.querySelector("footer").scrollIntoView({ behavior: "smooth", block: "start" })
+}
+
+if (window.location.pathname.includes("index.html")) document.querySelector("#home").classList.add("nav-active")
+else if (window.location.pathname.includes("contact.html")) document.querySelector("#contact").classList.add("nav-active")
+else if (window.location.pathname.includes("products.html")) document.querySelector("#products").classList.add("nav-active")
+else document.querySelector("#home").classList.add("nav-active")
