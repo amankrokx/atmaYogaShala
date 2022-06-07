e946302cdfae847d7cf62ea9d66fc255a627d769
Aman Kumar <amankumar.spj410@gmail.com>
2022-06-08 00:00:10 +0530

    updated aboutus and ui improvements

--- public/common.js
+++ public/common.js
@@ -6,4 +6,5 @@ document.querySelector("#sitemap").onclick = e => {
 if (window.location.pathname.includes("index.html")) document.querySelector("#home").classList.add("nav-active")
 else if (window.location.pathname.includes("contact.html")) document.querySelector("#contact").classList.add("nav-active")
 else if (window.location.pathname.includes("products.html")) document.querySelector("#products").classList.add("nav-active")
+else if (window.location.pathname.includes("about.html")) document.querySelector("#about").classList.add("nav-active")
 else document.querySelector("#home").classList.add("nav-active")
